0eda7aad07af64944b8137b342bd31a3c9727405
Sanjay <sanjaytheeng007@gmail.com>
2025-02-21 16:15:22 +0545

    transaction type

--- p_account/models/placement_account_inherit.py
+++ p_account/models/placement_account_inherit.py
@@ -1,13 +1,9 @@
-
-
 from odoo import models, fields, api
 
 class PlacementTransactionType(models.Model):
     _name = 'placement.transaction.type'
 
     name = fields.Char(string="Transaction Type", required=True)
-
-
 
 class AccountPaymentInherit(models.Model):
     _inherit = 'account.payment'
@@ -19,5 +15,11 @@ class AccountPaymentInherit(models.Model):
         context={'create': False, 'edit': False},  # Disables creation & editing in selection
         required=True
     )
+
+
+
+
+
+
 
 
