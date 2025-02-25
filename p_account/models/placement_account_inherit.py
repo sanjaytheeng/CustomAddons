33cbf31172dd93082f2765ebf11b5ebc7b14b834
Sanjay <sanjaytheeng007@gmail.com>
2025-02-25 11:36:13 +0545

    add field in payment register

--- p_account/models/placement_account_inherit.py
+++ p_account/models/placement_account_inherit.py
@@ -16,6 +16,27 @@ class AccountPaymentInherit(models.Model):
         required=True
     )
 
+class AccountPaymentRegisterInherit(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    transaction_type = fields.Many2one(
+        'placement.transaction.type',
+        string="Transaction Type",
+        domain=[],  # Ensures all records are selectable
+        context={'create': False, 'edit': False},  # Disables creation & editing in selection
+        required=True
+    )
+
+    def action_create_payments(self):
+        res = super(AccountPaymentRegisterInherit, self).action_create_payments()
+
+        # Get the created payments by checking payments linked to the selected invoices
+        payments = self.env['account.payment'].search([('ref', '=', self.communication)])
+
+        if payments:
+            payments.write({'transaction_type': self.transaction_type.id})
+
+        return res
 
 
 
